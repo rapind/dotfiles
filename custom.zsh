1210246d8bb7a1e2d3e1a32dba458c020a7e1e56
Dave Rapin <dave@rapin.com>
2013-07-19 19:51:05 +0000

    tweaks to zsh

--- custom.zsh
+++ custom.zsh
@@ -2,10 +2,8 @@
 
 unsetopt correct_all
 
-alias l='ls -lah' # l for list style, a for all including hidden, h for human readable file sizes
-alias c='clear' # shortcut to clear your terminal
-alias ..='cd ..' # move up 1 dir
-alias vi="vim"
+alias c='clear'
+alias ..='cd ..'
+alias be='bundle exec'
 
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
-
+eval "$(rbenv init -)"
