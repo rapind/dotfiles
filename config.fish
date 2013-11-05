9829a26d62bcf90349e5524b071a334b593df2b8
Dave Rapin <dave@rapin.com>
2013-11-04 19:52:51 -0500

    added fish config

---
+++ config.fish
@@ -0,0 +1,36 @@
+function c
+  clear
+end
+
+function l
+  ls -all
+end
+
+function be
+  bundle exec
+end
+
+function mm
+  bundle exec middleman -p 3000
+end
+
+function rs
+  bundle exec rails server
+end
+
+function rc
+  bundle exec rails console
+end
+
+function vi
+  mvim
+end
+
+function vim
+  mvim
+end
+
+function bower
+  noglob bower
+end
+
